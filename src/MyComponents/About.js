cc4cbb58e2c518f45e787418d97d288e0e97fbea
Tejas Lolage <tejaslolage@gmail.com>
2022-09-07 02:36:36 +0530

    Final commit. Successfully built the production level build of the web app. Resolved all errors.

---
+++ src/MyComponents/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const About = () => {
+  return (
+    <div>
+      This is an about component
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet
+        reiciendis accusamus, vel sapiente necessitatibus tenetur deserunt
+        molestiae qui illum maxime placeat repudiandae minus incidunt voluptate,
+        blanditiis ipsum! Ipsa, vero vitae?
+      </p>
+    </div>
+  );
+};
