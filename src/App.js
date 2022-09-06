1abc6f84ce8ecb4aafdaacf3cb8a93921c919fa3
Tejas Lolage <tejaslolage@gmail.com>
2022-09-06 22:41:59 +0530

    Made use of useState method in React for deletion of items in runtime memory

--- src/App.js
+++ src/App.js
@@ -3,15 +3,23 @@ import './App.css';
 import Header from './MyComponents/Header';
 import { Footer } from './MyComponents/Footer';
 import { Todo } from './MyComponents/Todo';
-import { IndividualTodo } from './MyComponents/IndividualTodo';
+import React, { useState } from 'react';  // This is the method used to delete values in a list in ReactJS.
 
 function App() {
 
-  const onDelete = (todo)=>{
-    console.log("I am onDelete of todo", todo)
+  const onDelete = (todo) => {
+    console.log("I am onDelete of todo", todo);
+    // In React, deleting this way does not work
+    // let index = todos.indexOf(todo);  // This is the way the items are deleted from 
+    // todos.splice(index, 1);           // runtime variable memory in other frameworks than React.
+    setTodos(todos.filter((e) => {
+      return e !== todo;
+    }))
+
   }
 
-  let todos = [
+  const [todos, setTodos] = useState([
+
     {
       sno: 1,
       title: "Go to the market",
@@ -27,11 +35,12 @@ function App() {
       title: "Go to the ghat",
       desc: "You have to go to the market to buy jadibuti.",
     }
-  ]
+  ]);
+
   return (
     <>
       <Header title="Todo List" about={true} />
-      <Todo todos={todos} onDelete={onDelete}/>
+      <Todo todos={todos} onDelete={onDelete} />
       <Footer />
     </>
   );
